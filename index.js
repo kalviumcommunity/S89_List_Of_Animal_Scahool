a71fbbd18d5b59454a0eda069e9a01deb626af64
DevWebAbhi <tiwariabhishek889912@gmail.com>
2025-03-18 10:07:58 +0530

    Assignment Done

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
 const app = express();
 
 app.get("/ping",(req,res)=>{
     resizeBy.send("pong!");
 })
 
-app.listen(8080,()=>{
+app.listen(8080,async()=>{
     try {
+        await mongoose.connect(`mongodb+srv://abhishektiwari136136:${MONGO_PASSWORD}@cluster0.pxcmv.mongodb.net/`);
         console.log("Server connected sucessfully");
     } catch (error) {
         console.log(error)
