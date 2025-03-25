4924cb9ba2798c8606b902ee326ca27831ab75b2
DevWebAbhi <tiwariabhishek889912@gmail.com>
2025-03-25 11:17:43 +0530

    Task done

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
+
+const routes = require("./routes");
+
+app.use(express.json());
 
 app.get("/ping",(req,res)=>{
     res.send("pong!");
 })
 
+app.use("/animal-school",routes);
+
 app.listen(8080,async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(MONGO_URI);
         console.log("Server connected sucessfully");
     } catch (error) {
         console.log(error)
